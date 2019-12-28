b4ad29d20971fa568ab001eed164bfc804419e49
Nobu <rockmusic2090@gmail.com>
2019-12-27 18:02:08 -0800

    Finished sign in handleSubmit method

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/FormInput";
 import CustomBtn from "../custom-button/CustomBtn";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./SignIn.scss";
 
@@ -12,8 +12,17 @@ class SignIn extends React.Component {
     password: ""
   };
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
